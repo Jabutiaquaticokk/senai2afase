9897ddc09a6b09c7cc2ee7094d079a3e9028ae08
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-08-19 20:54:45 -0300

    semana 2

--- revisao-javascript/scripts/variaveis.js
+++ revisao-javascript/scripts/variaveis.js
@@ -1,5 +1,21 @@
 function exemplo(){
-    console.log('Olá, Mundo!');
+    console.log('funfa');
+
+    const nome='José';
+    const idade=17;
+    const possuiFaculdade=false;
+    const time='BFR';
+    const novoObjeto= {}
+    const hoje= new Date();
+
+    console.log('Typeof nome: ', typeof nome);
+    console.log('Typeof idade: ', typeof idade);
+    console.log('Typeof possuiFaculdade: ',  typeof possuiFaculdade);
+    console.log('Typeof novoObjeto: ', typeof novoObjeto);
+    console.log('Typeof hoje: ', typeof hoje);
+
+    
+    
 }
 
 export default exemplo;
