04f4788bcab43d698dfe53ada6a0ad635e1c720e
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-08-26 19:23:12 -0300

    semana 2

--- revisao-javascript/scripts/outros/cond.js
+++ revisao-javascript/scripts/outros/cond.js
@@ -1,6 +1,15 @@
 function exemplosCondicionais(){
     console.log('cond.js rodando');
     
+    const possuiGraduacao = true;
+    const possuiDoutorado = false;
+
+    if(possuiGraduacao) {
+    console.log('Possui graduação');
+    } else {
+    console.log('Não possui graduação');
+    }
+
 }
 
 export default exemplosCondicionais;
