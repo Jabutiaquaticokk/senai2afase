1ce9258d84042aa16b1c56f8f5d4a13f574f6b2f
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-08-26 20:41:46 -0300

    semana 3(datas)

--- revisao-javascript/main.js
+++ revisao-javascript/main.js
@@ -1,8 +1,9 @@
 import exemplo from "./scripts/variaveis.js";
 import exemplosCondicionais from "./scripts/outros/cond.js";
-
+import exemplosDatas from "./scripts/outros/datas.js";
 
 document.getElementById('btn-var').addEventListener('click',exemplo);
 
 document.getElementById('btn-cond').addEventListener('click',exemplosCondicionais);
 
+document.getElementById('btn-datas').addEventListener('click',exemplosDatas);
---
+++ revisao-javascript/scripts/outros/datas.js
@@ -0,0 +1,10 @@
+function exemplosDatas(){
+    console.log('datas.js em execução...');
+    const agora= new Date();
+
+    console.log(agora);
+
+    
+};
+
+export default exemplosDatas;
